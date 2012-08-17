ab06f6d26c9b2f08acd46b0e66289963ae386268
Michael Savage <mikejsavage@gmail.com>
2012-08-17 22:58:06 +0100

    Condensed hello example

--- examples/hello/pages/index.lua
+++ examples/hello/pages/index.lua
@@ -1,18 +1,7 @@
 return {
 	get = function( request, name )
-		local visits
-
-		if request.cookies.counter then
-			visits = request:setCookie( "counter", request.cookies.counter + 1, { expiresIn = 3600 } )
-		else
-			visits = request:setCookie( "counter", 1, { expiresIn = 3600 } )
-		end
-
-		if request.session.visits then
-			request.session.visits = request.session.visits + 1
-		else
-			request.session.visits = 1
-		end
+		local visits = request:setCookie( "counter", ( request.cookies.counter or 0 ) + 1, { expiresIn = 3600 } )
+		request.session.visits = ( request.session.visits or 0 ) + 1
 
 		request:render( "hello", name or "you", visits, request.session.visits )
 	end,
